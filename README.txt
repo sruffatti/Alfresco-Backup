c2995e493eca41747de2924e7603dbc6a0078fd7
Sean R. Ruffatti <seanruffatti@yahoo.com>
2018-01-20 16:39:53 -0600

    restructured, added exception handling, and enchanced readme

--- README.txt
+++ README.txt
@@ -1,11 +1,51 @@
 Alfresco-Linux Backup Script
+Author: Sean Ruffatti
 
 This script was built to backup the current instance of Alfresco on DePaul's server.
 
 Overview
- - There are 5 steps necessary to have a valuable backup of Alfresco.
+ - There are 5 steps necessary to backup Alfresco.
  1. Stop Tomcat services
  2. Run pg_dumpall, store .sql file in target folder.
  3. Copy 'alf_data' or dir.root folder to target folder. 
  4. Tar gzip target folder.
- 5. Start Tomcat services.
+ 5. Restart Tomcat services.
+
+Methods
+1. stopTomcat()
+	stopTomcat() stops tomcat by changing to the '/alfresco-community' directory and running the command './alfresco.sh stop tomcat' 
+	The command to stop tomcat is stored in the variable 'stopTomcatCommand'. If this process errors, the script will not proceed with the backup, and log the error. 
+
+2. startTomcat()
+	startTomcat() starts tomcat by changing to the '/alfresco-community' directory and running the command './alfresco.sh start tomcat'.
+	The command to start tomcat is stored in the variable 'startTomcatCommand'. If this process errors, the error will be logged and tomcat will not resume.
+
+3. buildTimeStampDirectory()
+	buildTimeStampDirectory() is responsible for creating the targer folder. First it checks if the folder exists, if it does not exist, then it will create one.
+	The time stamp is assigned to the 'timeStampDir' variable using the datetime package. Formatted 'YYYYMMDD' 
+
+4. checkForTimeStampDB()
+	checkForTimeStampDB() is responsible for checking if there is a .sql file in the target directory, and if there is, remove the .sql file. 
+	'pgFile' is the variable storing the time stamped sql file. Refer to the timeStampDB variable. Formated 'YYYYMMDD_HHMMSS'
+
+5. dumpDatabase()
+	dumpDatabase() is responsible for running the pg_dumpall command. First, we change to the '/alfresco-community/postgresql/bin'.
+	Then we run the command -  'PGPASSWORD= "mhc123" pg_dumpall -c -h localhost -p 5432 -U postgres -f /home/alfresco/alfresco-backup/<time-stamp>/dumped.sql'
+		PGPASSWORD creates an environmental variable and you assign it a password. This is necessary so the script can access all the databases. 
+		pg_dumpall writes all postgres databases of a cluster into one script file.
+		-c includes SQL commands to clean databases before recreating them.
+		-h Specifies the host name of the machine on which the database server is running. We use 'localhost'
+		-p Specifies the TCP port
+		-U User name to connect as.
+		-f Send output to the specified file. If this is omitted, the standard output is used.
+	
+	If the pg_dumpall command fails, tomcat will restart, the backup will stop and the error will be logged. 
+
+6. copyAlfData()
+	copyAlfData() is responsible for taking a copy of alfresco's contentstore directory and placing the copy in the target directory. 
+
+7. zipTargetFolder()
+	zipTargetFolder() is responsible for zipping the target folder. It first creates a targz folder, then adds the backup and contentstore and then closes the file.
+
+8. remomveDirectory()
+	removeDir() is responbile for removing the non-zipped folder that holds the backup and the contentstore files.
--- alf_backup.py
+++ alf_backup.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Jan  7 18:31:13 2018
+Updated on Sat Jan 20 15:53:00 2018
 
 NOTICE: Before running, please make sure 'postgres-client' is installed. 
 to install run: 'sudo apt-get install postgresql-9.1' or current version'
@@ -8,90 +9,122 @@ to install run: 'sudo apt-get install postgresql-9.1' or current version'
 @author: Sean Ruffatti
 """
 
-import os
-import datetime
-import shutil
-import tarfile
+import os, sys, datetime, shutil, tarfile, subprocess
 
 #Base directory
 currentWorkingDirectory = os.getcwd() #'home/alfresco/scripts'
+
 
 #Time stamp
 timeStampDir = datetime.datetime.now().strftime('%Y%m%d')
 timeStampDB = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
 
+
 #Configure Paths
 homeDir  = '/home'
 userDir = homeDir + '/alfresco'
 alfrescoRoot = userDir + '/alfresco-community'
-dirRoot = alfrescoRoot + '/alf_data'
-contentStorePath = dirRoot + '/contentstore'
-postgresDatabasePath = alfrescoRoot + '/postgresql/bin'
+alfrescoData = alfrescoRoot + '/alf_data'
+postgresDatabaseBin = alfrescoRoot + '/postgresql/bin'
+alfrescoBackupDir = userDir + '/alfresco-backup/'
+pgTargetDir = alfrescoBackupDir + timeStampDir +"/"
+pgTargetDirContentStore = pgTargetDir + "/contentstore"
+pgFile = pgTargetDir + timeStampDB + '.sql'
 
-#Postgresql Variables
+
+#Postgresql pg_dumpall Variables
 pgHost = 'localhost'
 pgPort = '5432'
 pgUser = 'postgres'
 pgPasswd = '\"mhc123\"'
-pgBackupDir = userDir + '/alfresco-backup/'
-pgTargetDir = pgBackupDir + timeStampDir +"/"
-pgTargetDirContentStore = pgTargetDir + "/contentstore"
-pgFileName = pgTargetDir + timeStampDB + '.sql'
+
 
 #Terminal commands
 stopTomcatCommand = './alfresco.sh stop tomcat'
 startTomcatCommand = './alfresco.sh start tomcat'
 # 'PGPASSWORD= "mhc123" pg_dumpall -c -h 127.0.0.1 -p 5432 -U postgres -f /home/alfresco/alfresco-backup/<time-stamp>/dumped.sql'
-pgDumpCommand = 'PGPASSWORD=' + pgPasswd + ' pg_dumpall -c -h '+ pgHost +' -p '+ pgPort +' -U '+ pgUser +' -f '+ pgFileName  
+pgDumpCommand = 'PGPASSWORD=' + pgPasswd + ' pg_dumpall -c -h '+ pgHost +' -p '+ pgPort +' -U '+ pgUser +' -f '+ pgFile  
 
-#Stop Tomcat Services
-os.chdir(alfrescoRoot) # home/alfresco/alfresco-community'
-os.system(stopTomcatCommand)
-print('\tTomcat services stopped')
 
-#Build time stamp directory
-print('\tCreating time stamp directory')
-os.chdir(pgBackupDir)
-if not os.path.exists(pgTargetDir):
-    os.mkdir(timeStampDir)
-    
-#Check for time Stamp DB before 
-try:
-    os.remove(pgFileName)
-except OSError:
-    pass
+#Method for stopping tomcat services
+def stopTomcat():
+    os.chdir(alfrescoRoot) # home/alfresco/alfresco-community'
+    returnCode = subprocess.call(stopTomcatCommand, shell=True)
+    if returnCode != 0:
+        print('\tTomcat could not be stopped. Exiting script.')
+        sys.exit('Tomcat could not be stopped.')
+    else:
+        print('\tTomcat services stopped')
 
-#Postgres Dump 
-os.chdir(postgresDatabasePath)
-print('\tRunning pg_dumpall command')
-os.system(pgDumpCommand)
-print('\tDatabases were dumped')
+#Method for starting tomcat services    
+def startTomcat():
+    os.chdir(alfrescoRoot) # home/alfresco/alfresco-community
+    returnCode = subprocess.call(startTomcatCommand, shell=True)
+    if returnCode != 0:
+        print('\tTomcat failed to start. Start it manually.')
+    else:
+        print('\tTomcat is running.')
+        
+#Method to build time stamp directory
+def buildTimeStampDirectory():
+    print('\tCreating time stamp directory')
+    os.chdir(alfrescoBackupDir)
+    if not os.path.exists(pgTargetDir):
+        os.mkdir(timeStampDir)
+  
+#Method to check for time Stamp DB file
+def checkForTimeStampDB():
+    try:
+        os.remove(pgFile)
+    except OSError:
+        pass
 
-#Copy alfresco data folder to target folder
-os.chdir(dirRoot)
-print('\tCopying contentstore folder to target directory')
-contentStore = './contentstore'
-shutil.copytree(contentStore, pgTargetDirContentStore)
+#Method to issue postgres pg_dumpall command
+def dumpDatabase():
+    os.chdir(postgresDatabaseBin)
+    print('\tRunning pg_dumpall command')
+    returnCode = subprocess.call(pgDumpCommand, shell=True)
+    if returnCode != 0:
+        print('\tDatabase dump failed. Starting tomcat.')
+        startTomcat()
+        sys.exit('Database dump failed. Exiting script.')
+    else:
+        print('\tDatabase dump was successful.')
+   
+#Method to copy contentstore directory to target directory     
+def copyAlfData():
+    os.chdir(alfrescoData)
+    print('Copying contentstore folder to target directory')
+    src = './contentstore'
+    try:
+        shutil.copytree(src, pgTargetDirContentStore)
+    except IOError:
+        print('Unable to copt file.')
 
-#Tar gzip target folder.
-os.chdir(pgBackupDir)
-print('\tTar zipping target directory')
-currDir = './' + timeStampDir
-zippedFile = timeStampDB + '.tar.gz'
-tar = tarfile.open(zippedFile, 'w:gz')
-tar.add(currDir)
-tar.close()
+#method to zip target folder.   
+def zipTargetFolder():
+    os.chdir(alfrescoBackupDir)
+    print('\tZipping target directory')
+    currDir = './'+ timeStampDir
+    zippedFile = timeStampDB + '.tar.gz'
+    tar = tarfile.open(zippedFile, 'w:gz')
+    tar.add(currDir)
+    tar.close()
 
-#Remove unzipped directory and all of its contents
-shutil.rmtree(currDir)
+#method to remove original directory.
+def removeDirectory():
+    currDir = './'+ timeStampDir
+    shutil.rmtree(currDir)
 
-#Start Tomcat services
-print('\tStarting tomcat services')
-os.chdir(alfrescoRoot)
-os.system(startTomcatCommand)
-print('\tTomcat services started')
-print('\tAlfresco backup successful')
-
+#Backup Process
+stopTomcat()
+buildTimeStampDirectory()
+checkForTimeStampDB()
+dumpDatabase()
+copyAlfData()
+zipTargetFolder()
+removeDirectory()
+startTomcat()
 
 
 
